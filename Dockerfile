487f7084ed9e607178308d4bc7109cba5f20b2fb
v.la <v.la@live.cn>
2016-08-12 00:41:22 +0800

    build .netcore with  bower glup

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
-FROM microsoft/dotnet:1.0.0-preview2-sdk
-#FROM johnwu/debian:jessie
+#FROM microsoft/dotnet:1.0.0-preview2-sdk
+FROM johnwu/debian:jessie
 MAINTAINER JohnWu "v.la@live.cn"
 
-ENV DOTNET_VERSION=1.0.0-preview2 \
+ENV DOTNET_SDK_VERSION=1.0.0-preview2 \
+    NUGET_XMLDOC_MODE=skip \  
     NODE_VERSION=4.4.7 \
     NPM_CONFIG_LOGLEVEL=info \
     DOTNET_SETUP_DIR=/usr/src/dotnet-build
 
+ARG NPM_REGISTRY="--registry=https://registry.npm.taobao.org"
+
 COPY setup/ ${DOTNET_SETUP_DIR}/
 
 RUN bash ${DOTNET_SETUP_DIR}/install.sh
-
--- setup/install.sh
+++ setup/install.sh
@@ -1,9 +1,49 @@
 #!/bin/bash
+set -e
 
-RUNTIME_DEPENDENCIES=""
-BUILD_DEPENDENCIES="xz-utils"
+DOTNET_SDK_DOWNLOAD_URL="https://go.microsoft.com/fwlink/?LinkID=809130"
 
-#gpg keys listed at https://github.com/nodejs/node
+RUNTIME_DEPENDENCIES="libc6 \
+        libcurl3 \
+        libgcc1 \
+        libgssapi-krb5-2 \
+        libicu52 \
+        liblttng-ust0 \
+        libssl1.0.0 \
+        libstdc++6 \
+        libunwind8 \
+        libuuid1 \
+        zlib1g "
+BUILD_DEPENDENCIES="xz-utils "
+
+apt-get update 
+apt-get install --no-install-recommends --no-install-suggests -y ${RUNTIME_DEPENDENCIES} ${BUILD_DEPENDENCIES}
+
+##### ##### ##### ##### ##### ##### ##### ##### ##### #####
+#                           
+#               Install .Net Core SDK   
+#
+#  ${DOTNET_SETUP_DIR}/project.json文件主要用来下载类库到本地
+#  预先下载相应的依赖类库,可以加快每次编译需要的下载时间  
+#  PS：还可以挂载到宿主机类库到/root/.nuget/package目录下
+#                          
+##### ##### ##### ##### ##### ##### ##### ##### ##### #####
+wget -cq ${DOTNET_SDK_DOWNLOAD_URL} -O "${DOTNET_SETUP_DIR}/dotnet.tar.gz"
+mkdir -p /usr/share/dotnet
+tar -zxf "${DOTNET_SETUP_DIR}/dotnet.tar.gz" -C /usr/share/dotnet
+ln -sf /usr/share/dotnet/dotnet /usr/bin/dotnet
+#缓存基础依赖类库到本地
+dotnet restore ${DOTNET_SETUP_DIR}
+rm -rf /tmp/NuGetScratch
+
+##### ##### ##### ##### ##### ##### ##### ##### ##### #####
+#                           
+#            Install Front Building Support
+# 
+#  1.初始安装的包将会存放在/user/local/lib/node_modules
+#  2.外部挂载地址存放在/usr/local/lib/node_external_module
+#                       
+##### ##### ##### ##### ##### ##### ##### ##### ##### #####
 for key in \
     9554F04D7259F04124DE6B476D5A82AC7E37093B \
     94AE36675C464D64BAFA68DD7434390BDBE9B9C5 \
@@ -17,21 +57,20 @@ for key in \
     gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
   done
 
-apt-get update 
-apt-get install --no-install-recommends --no-install-suggests -y ${RUNTIME_DEPENDENCIES} ${BUILD_DEPENDENCIES}
-
-wget -cq "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.xz" -P ${DOTNET_SETUP_DIR}/src
-wget -cq "https://nodejs.org/dist/v$NODE_VERSION/SHASUMS256.txt.asc" -P ${DOTNET_SETUP_DIR}/src
-
-cd ${DOTNET_SETUP_DIR}/src
+cd ${DOTNET_SETUP_DIR}/
+wget -cq "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.xz" -P ${DOTNET_SETUP_DIR}/
+wget -cq "https://nodejs.org/dist/v$NODE_VERSION/SHASUMS256.txt.asc" -P ${DOTNET_SETUP_DIR}/
 gpg --batch --decrypt --output "SHASUMS256.txt" "SHASUMS256.txt.asc"
 grep "node-v$NODE_VERSION-linux-x64.tar.xz\$" "SHASUMS256.txt" | sha256sum -c -
-tar -xJf "node-v$NODE_VERSION-linux-x64.tar.xz" -C /usr/local --strip-components=1
+tar -xJf "node-v$NODE_VERSION-linux-x64.tar.xz" -C /usr/local --strip=1
 
-export NODE_PATH="/usr/lib/npm;/usr/local/lib/node_modules"
+mkdir -p /usr/local/lib/node_external_module
+export NODE_PATH="/user/local/lib/node_modules;/usr/local/lib/node_external_module"
 
+#安装必要包
+npm install $(cat ${DOTNET_SETUP_DIR}/npm.txt) -g ${NPM_REGISTRY}
 
 # cleanup
 apt-get purge -y --auto-remove ${BUILD_DEPENDENCIES}
-rm -rf ${DOTNET_SETUP_DIR}/
 rm -rf /var/lib/apt/lists/*
+rm -rf ${DOTNET_SETUP_DIR}/
